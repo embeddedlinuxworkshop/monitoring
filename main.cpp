e0e44b7e270bdad0806edfa1fd6db72dbaefcc5b
embeddedlinuxworkshop <139722851+embeddedlinuxworkshop@users.noreply.github.com>
2023-12-30 04:31:29 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,27 @@
 #include <syslog.h>
+#include <thread>
+#include <chrono>
 
 int main() {
     // Open syslog with a specified ident (usually the program name) and logging options
-    openlog("my_cpp_app", LOG_PID | LOG_CONS, LOG_USER);
 
-    // Log a message with severity LOG_INFO
-    syslog(LOG_INFO, "This is an informational message");
+    while (true)
+    {
+        openlog("my_cpp_app", LOG_PID | LOG_CONS, LOG_USER);
+        
+        // Log a message with severity LOG_INFO
+        syslog(LOG_INFO, "This is an informational message");
+    
+        // Log a message with severity LOG_ERR
+        syslog(LOG_ERR, "An error occurred!");    
 
-    // Log a message with severity LOG_ERR
-    syslog(LOG_ERR, "An error occurred!");
+
+
+        // Sleep for 3 seconds
+        std::this_thread::sleep_for(std::chrono::seconds(3));
+
+    }
+   
 
     // Close syslog when done
     closelog();
